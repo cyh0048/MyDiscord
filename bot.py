90b6def335a5cae271970324f97a7e5fef90e5d5
cyh0048 <comjang12@naver.com>
2020-02-01 16:59:43 +0900

    asd

--- bot.py
+++ bot.py
@@ -102,7 +102,7 @@ async def on_message(message):
         await message.channel.send(embed=inf)
 
     @client.command()
-    async def clear(ctx, amount):
+    async def clear(ctx, amount=5):
         await ctx.channel.purge(limit=amount)
 
 
