14bd4c363e1ede40a315dd256c585676642b921c
cyh0048 <comjang12@naver.com>
2020-02-01 12:02:57 +0900

    asd

--- bot.py
+++ bot.py
@@ -19,29 +19,26 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.content.startswith("!운영진"):
-        embed = discord.Embed(color=0xF7FE2E)
-        embed.add_field(name="이름", value="영환", inline=True)
-        embed.add_field(name="좀비고닉네임", value="플2부터다시", inline=True)
-        embed.add_field(name="운영", value="단무지", inline=True)
-        embed.add_field(name="역할", value="디코 봇 제작", inline=True)
+        embed = discord.Embed(title ="단무지", color=0xF7FE2E)
+        embed.add_field(name="이름", value="영환", inline=False)
+        embed.add_field(name="좀비고닉네임", value="플2부터다시", inline=False)
+        embed.add_field(name="역할", value="디코 봇 제작", inline=False)
         embed.set_thumbnail(
-            url="https://cdn.discordapp.com/avatars/283193429574418432/9905fb1f4fbdd4ea25800aea045bebce.png?size=128")
+            url=message.author.avatar_url)
 
-        embed1 = discord.Embed(color=0xDF0101)
-        embed1.add_field(name="이름", value="진우", inline=True)
-        embed1.add_field(name="좀비고닉네임", value="카트충지누", inline=True)
-        embed1.add_field(name="운영", value="클마", inline=True)
-        embed1.add_field(name="역할", value="가입문의", inline=True)
+        embed1 = discord.Embed(title="클마", color=0xDF0101)
+        embed1.add_field(name="이름", value="진우", inline=False)
+        embed1.add_field(name="좀비고닉네임", value="카트충지누", inline=False)
+        embed1.add_field(name="역할", value="가입문의", inline=False)
         embed1.set_thumbnail(
-            url="https://cdn.discordapp.com/avatars/542574486231318529/0f55203e2a3916498487d5fe86ed1af0.png?size=128")
+            url=message.author.avatar_url)
 
-        embed2 = discord.Embed(color=0x00ff00)
-        embed2.add_field(name="이름", value="상서", inline=True)
-        embed2.add_field(name="좀비고닉네임", value="김상서", inline=True)
-        embed2.add_field(name="운영", value="부마", inline=True)
-        embed2.add_field(name="역할", value="drawing picture", inline=True)
+        embed2 = discord.Embed(title="부마", color=0x00ff00)
+        embed2.add_field(name="이름", value="상서", inline=False)
+        embed2.add_field(name="좀비고닉네임", value="김상서", inline=False)
+        embed2.add_field(name="역할", value="drawing picture", inline=False)
         embed2.set_thumbnail(
-            url="https://cdn.discordapp.com/avatars/652929821692919837/f5424135d42dd61419f9544c1b44ec54.png?size=128")
+            url=message.author.avatar_url)
 
         await message.channel.send(embed=embed1)
         await message.channel.send(embed=embed2)
@@ -58,22 +55,22 @@ async def on_message(message):
                 file.save("경고.xlsx")
                 if sheet["B" + str(i)].value == 2:
                     dogsae2 = discord.Embed(color=0xDF0101)
-                    dogsae2.add_field(name="디스코드 이름", value=message.author.name, inline=True)
-                    dogsae2.add_field(name="디스코드 고유 아이디", value=message.author.id, inline=True)
-                    dogsae2.add_field(name="사용언어", value=message.content, inline=True)
-                    dogsae2.add_field(name="상태", value="투아웃", inline=True)
+                    dogsae2.add_field(name="디스코드 이름", value=message.author.name, inline=False)
+                    dogsae2.add_field(name="디스코드 고유 아이디", value=message.author.id, inline=False)
+                    dogsae2.add_field(name="사용언어", value=message.content, inline=False)
+                    dogsae2.add_field(name="상태", value="투아웃", inline=False)
                     dogsae2.set_thumbnail(url=message.author.avatar_url)
-                    # await bot.get_channel(int(672143900940697600)).send(embed=dogsae2)
+                    await bot.get_channel(int(672143900940697600)).send(embed=dogsae2)
                     await message.channel.send(embed=dogsae2)
                     break
                 elif sheet["B" + str(i)].value == 3:
                     dogsae3 = discord.Embed(color=0xDF0101)
-                    dogsae3.add_field(name="디스코드 이름", value=message.author.name, inline=True)
-                    dogsae3.add_field(name="디스코드 고유 아이디", value=message.author.id, inline=True)
-                    dogsae3.add_field(name="사용언어", value=message.content, inline=True)
-                    dogsae3.add_field(name="상태", value="삼진아웃", inline=True)
+                    dogsae3.add_field(name="디스코드 이름", value=message.author.name, inline=False)
+                    dogsae3.add_field(name="디스코드 고유 아이디", value=message.author.id, inline=False)
+                    dogsae3.add_field(name="사용언어", value=message.content, inline=False)
+                    dogsae3.add_field(name="상태", value="삼진아웃", inline=False)
                     dogsae3.set_thumbnail(url=message.author.avatar_url)
-                    # await bot.get_channel(int(672143900940697600)).send(embed=dogsae3)
+                    await bot.get_channel(int(672143900940697600)).send(embed=dogsae3)
                     await message.channel.send(embed=dogsae3)
                     await message.guild.ban(author)
                     break
@@ -82,19 +79,26 @@ async def on_message(message):
                 sheet["B" + str(i)].value = 1
                 file.save("경고.xlsx")
                 dogsae = discord.Embed(color=0xDF0101)
-                dogsae.add_field(name="디스코드 이름", value=message.author.name, inline=True)
-                dogsae.add_field(name="디스코드 고유 아이디", value=message.author.id, inline=True)
-                dogsae.add_field(name="사용언어", value=message.content, inline=True)
-                dogsae.add_field(name="상태", value="원아웃", inline=True)
+                dogsae.add_field(name="디스코드 이름", value=message.author.name, inline=False)
+                dogsae.add_field(name="디스코드 고유 아이디", value=message.author.id, inline=False)
+                dogsae.add_field(name="사용언어", value=message.content, inline=False)
+                dogsae.add_field(name="상태", value="원아웃", inline=False)
                 dogsae.set_thumbnail(url=message.author.avatar_url)
-                # await bot.get_channel(int(672143900940697600)).send(embed=dogsae)
+                await bot.get_channel(int(672143900940697600)).send(embed=dogsae)
                 await message.channel.send(embed=dogsae)
                 break
             i += 1
 
     if message.content.startswith("!내정보"):
         inf = discord.Embed(title=message.author.name + "님의 정보" , color=0x013ADF)
-        inf.add_field(name="디스코드 이름", value=message.author.name, inline=True)
+        inf.add_field(name="디스코드 이름", value=message.author.name, inline=False)
+        inf.add_field(name="디스코드 가입 날짜", value=message.author.created_at, inline=False)
+        inf.add_field(name="서버 가입 날짜", value=message.author.joined_at, inline=False)
+        inf.add_field(name="디스코드 역할", value=message.author.top_role, inline=False)
+        inf.set_thumbnail(url=message.author.avatar_url)
+
+        await message.channel.send(embed=inf)
+
 
 
 access_token = os.environ["BOT_TOKEN"]
